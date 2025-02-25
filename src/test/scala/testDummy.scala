d8dfd4d1e9f1fa0545cd6555ce932fdd05d6d27b
NFM-ISEP <43273289+NFM-ISEP@users.noreply.github.com>
2025-02-25 12:00:32 +0000

    Update testDummy.scala

--- src/test/scala/testDummy.scala
+++ src/test/scala/testDummy.scala
@@ -2,7 +2,7 @@ import scala.language.adhocExtensions
 import org.scalatest.funsuite.AnyFunSuite
 
 class testDummy extends AnyFunSuite:
-  test("Dummy test 01"):
+  test("Dummy test 02"):
     val input = "7"
     val expected = 7
     assert(Dummy.stringToInt(input) === expected)
